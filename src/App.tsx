5c9e589ec52a707bc84472e1ea4ad1b2a9b86c92
larocquedylan <dylancblarocque@gmail.com>
2023-12-18 19:16:43 -0800

    input working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import FileInput from './components/fileInput'
 
 function App() {
 
   return (
     <>
       <p> welcome to my web exif viewer app. </p>
+      <FileInput />
     </>
   )
 }
