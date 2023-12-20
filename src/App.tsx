60de49b82a8abc12d6a07d6354ebb9a6885daefc
larocquedylan <dylancblarocque@gmail.com>
2023-12-19 23:18:03 -0800

    import new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import './App.css'
-import FileInput from './components/fileInput'
+import FileInput from './components/Input/FileInput'
+import PhotoViewer from './components/Photo/Photo'
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  
+  const handleFileSelected = (file: File) => {
+    setSelectedFile(file);
+  };
+
 
   return (
     <>
       <p> welcome to my web exif viewer app. </p>
-      <FileInput />
+      <div className='App'>
+        <FileInput onFileSelected={handleFileSelected} />
+        <PhotoViewer file={selectedFile} />
+      </div>  
     </>
   )
 }
