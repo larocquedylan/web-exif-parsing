216f4a0ca32c1352fc8624755adeab374890e227
larocquedylan <dylancblarocque@gmail.com>
2024-01-31 23:27:43 -0800

    establish routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Ascii from './routes/Ascii';
+import Exif from './routes/Exif';
+import Home from './routes/Home';
+import { ImageProvider } from './components/ImageContent/ImageContext';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/ascii',
+    element: <Ascii />,
+  },
+  {
+    path: '/exif',
+    element: <Exif />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ImageProvider>
+      <RouterProvider router={router} />
+    </ImageProvider>
   </React.StrictMode>,
-)
+);
